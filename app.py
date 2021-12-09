405257677f61ff2e2bf13606a3e0e8035e35ab67
Elena Dorokhina <56249089+len8272@users.noreply.github.com>
2021-12-09 19:30:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def values(massage: telebot.types.Message):
     bot.reply_to(massage, text)
 
 @bot.message_handler(content_types=['text'])
-def convert(message: telebot.types.Message):
+def get_price(message: telebot.types.Message):
     try:
         values = message.text.split(' ')
 
