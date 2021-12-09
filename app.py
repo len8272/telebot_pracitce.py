27947d23e289096fedc34eb94b138876e5978e62
Elena Dorokhina <56249089+len8272@users.noreply.github.com>
2021-12-09 17:35:08 +0500

    app.py part

---
+++ app.py
@@ -0,0 +1,42 @@
+import telebot
+from config import keys, TOKEN
+from extensions import ConvertionException, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    text = 'Привествую вас.Чтобы начать работу введите комманду боту в следующем формате (через пробел): \n\n \
+<имя валюты цену которой он хочет узнать> \n \
+<имя валюты в которой надо узнать цену первой валюты> \n \
+<количество первой валюты>\n\n \
+Увидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(massage: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key))
+    bot.reply_to(massage, text)
+
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.get_price(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling()
